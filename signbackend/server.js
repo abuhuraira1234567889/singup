49762ed1b065e57929def44e39749da35f8b8eb6
abuhuraira1234567889 <hurairaabu098@gmail.com>
2022-03-22 13:21:22 +0500

    init

--- signbackend/server.js
+++ signbackend/server.js
@@ -1,16 +1,20 @@
-const express=require('express')
-const app=express()
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
-const routesUrls=require('./route/route')
-const cors=require('cors')
-dotenv.config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const routesUrls = require("./route/route");
+const cors = require("cors");
+dotenv.config();
 
-mongoose.connect(process.env.DATABASE_ACCESS,()=>console.log("db is connected"))
+mongoose.connect(process.env.DATABASE_ACCESS, () =>
+  console.log("db is connected")
+);
+app.get("/", () => {
+  console.log("running");
+});
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json()) 
-app.use(cors())
-
-app.use(routesUrls)
-app.listen(4000,()=>console.log("server is running"))
+app.use(routesUrls);
+app.listen(4000, () => console.log("server is running"));
